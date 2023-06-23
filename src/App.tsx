d4bc93f0d375cd2a8853df720f00d809fa8a6706
Brian <bstanton773@gmail.com>
2023-06-23 14:36:47 -0500

    Update isLoggedIn state to look at localStorage, update logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from "./components/Navigation";
 import AlertMessage from './components/AlertMessage';
@@ -8,15 +8,27 @@ import Register from './views/Register';
 import Container from 'react-bootstrap/Container'
 import UserType from './types/auth';
 import CategoryType from './types/category';
+import { getMe } from './lib/apiWrapper';
 
 
 export default function App() {
 
     // let isLoggedIn: boolean = false;
-    const [isLoggedIn, setLoggedIn] = useState(false)
+    const [isLoggedIn, setLoggedIn] = useState((localStorage.getItem('token') && new Date(localStorage.getItem('tokenExp') as string) > new Date()) || false)
     const [loggedInUser, setLoggedInUser] = useState<UserType|null>(null)
     const [message, setMessage] = useState<string|null>(null);
     const [category, setCategory] = useState<CategoryType>(null);
+
+    useEffect(() => {
+        const getLoggedInUser = async() => {
+            const token = localStorage.getItem('token')
+            const response = await getMe(token!)
+            setLoggedInUser(response.data!)
+        }
+        if (isLoggedIn){
+            getLoggedInUser()
+        }
+    }, [])
 
     const logUserIn = (user:UserType): void => {
         setLoggedIn(true);
@@ -27,6 +39,8 @@ export default function App() {
     const logUserOut = (): void => {
         setLoggedIn(false);
         setLoggedInUser(null);
+        localStorage.removeItem('token');
+        localStorage.removeItem('tokenExp');
         flashMessage('You have been logged out', 'info')
     }
 
