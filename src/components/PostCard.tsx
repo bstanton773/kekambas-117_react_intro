348773f33e2c021255aabefb34181cd81ee41db2
Brian <bstanton773@gmail.com>
2023-06-23 14:43:57 -0500

    Add Date Created to Post

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -7,13 +7,14 @@ type PostCardProps = {
 
 export default function PostCard({ post }: PostCardProps) {
     // console.log(post)
+    const date = new Date(post.dateCreated as string)
     return (
         <Card className='mt-3'>
             <Card.Body>
                 <Card.Title>{post.title}</Card.Title>
                 <Card.Text>{post.body}</Card.Text>
                 <Card.Subtitle>By {post.author?.firstName} {post.author?.lastName} </Card.Subtitle>
-                <Card.Text className='text-muted'>Date Created:</Card.Text>
+                <Card.Text className='text-muted'>Date Created: {date.toString()}</Card.Text>
             </Card.Body>
         </Card>
     )
